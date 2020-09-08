e4e0cc75c47d418a0cf9eabe47b4f72e6f7934d7
Jack <drownlifex@gmail.com>
2020-09-08 14:59:46 -0300

    second commit

--- src/middlewares/first.middleware.ts
+++ src/middlewares/first.middleware.ts
@@ -1,7 +1,6 @@
 import { Injectable, NestMiddleware } from '@nestjs/common';
-import { IncomingMessage, ServerResponse } from 'http';
 import { Request, Response } from 'express';
-
+import * as redis from "redis";
 
 
 @Injectable()
@@ -10,13 +9,31 @@ export class FirstMiddleware implements NestMiddleware {
     // console.log(req.rawHeaders[11])
     
     // console.log(req.url)
+  
+    
+    const client = redis.createClient();
+    
+    client.on("connect", function() {
+      console.log("You are now connected");
+    });
 
+    const existInRedis = (clave: any, value: any):boolean =>{
+      let temp = false;
 
+      client.get(clave, function(err, reply) {
+        if(reply === value){
+          temp = true;
+        }
+      });
+
+      return temp
+    }
 
     if(req.url !== "/"){
       res.send("KeKieresAserPrrrrrO ╭∩╮（︶_︶）╭∩╮")
     }else{
-      console.log(req.headers)
+      console.log(req.headers["token"])
+      // console.log(existInRedis( "token" , req.headers["token"]))
       next();
       
     }
